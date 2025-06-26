3e561df34d70cc4c0a38807e9160185310614ecf
Emma <MarmadileManteater@proton.me>
2025-06-26 09:56:36 -0400

    Fix jump counting twice on touch input

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,23 +2,27 @@ extends RigidBody2D
 
 var sprite: AnimatedSprite2D
 var alive: bool = true
+var is_jumping: bool = false
 signal dead
 
 func _enter_tree():
 	sprite = find_child("AnimatedSprite2D")
 
 func _input(event: InputEvent) -> void:
-	if (event.is_action_pressed("ui_accept") 
-	|| event is InputEventMouseButton
-	|| event is InputEventScreenTouch):
-		if (len(get_colliding_bodies()) != 0):
-			apply_central_force(Vector2(0, -18 * 1600))
-			sprite.animation = "jump_right"
+	if (!is_jumping):
+		if (event.is_action_pressed("ui_accept") 
+		or event is InputEventMouseButton
+		or event is InputEventScreenTouch):
+			if (len(get_colliding_bodies()) != 0):
+				is_jumping = true
+				apply_central_force(Vector2(0, -18 * 1600))
+				sprite.animation = "jump_right"
 
 func _physics_process(delta: float) -> void:
 	if (len(get_colliding_bodies()) != 0 and !sprite.is_playing()):
 		sprite.animation = "walk_right"
 		sprite.play()
+		is_jumping = false
 	if (position.y > 400):
 		emit_signal("dead")
 		alive = false
